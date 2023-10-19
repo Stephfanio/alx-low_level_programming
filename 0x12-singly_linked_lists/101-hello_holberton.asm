4d5b727d6c9688d845ca974b263c0c295e3fc389
Stephfanio <Stephanieorsule@gmail.com>
2023-10-19 08:00:07 -0700

    0x12-singly_linked_lists

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,22 +1,17 @@
-extern	printf		; the C function, to be called
+;; declare required c functions
+	extern printf		; the c function to be called
+	section .data		; data section initializing variables
+msg:	db 'Hello, Holberton', 0 ; string 0
+fmt:	db "%s", 10, 0		 ; printf format for new line and 0
 
-	        section .data		; Data section, initialized variables
-msg:		db "Hello, Holberton", 0 ; C string needs 0
-fmt:	    	db "%s", 10, 0          ; The printf format, "\n",'0'
-
-	        section .text		; Code section.
-
-	        global main		; the standard gcc entry point
-main:					; the program label for the entry point
-	        push    rbp		; set up stack frame, must be alligned
-
-		mov	rdi,fmt
-		mov	rsi,msg
-		mov	rax,0		; or can be  xor  rax,rax
-	        call    printf		; Call C function
-
-		pop	rbp		; restore stack
-
-		mov	rax,0		; normal, no error, return value
-		ret			; return
-
+	section .text		; code
+	global main		; gcc entry point
+main:				; program entry point
+	push rbp		;creating stack frame
+	mov rdi,fmt
+	mov rsi,msg
+	mov rax,0
+	call printf		; calling required function
+	pop rbp			; clean stack
+	mov rax,0		; error value
+	ret			; return
