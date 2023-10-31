7d01afc435e55bcf8b46b63d03e3fcf547ac0fd8
Stephfanio <Stephanieorsule@gmail.com>
2023-10-31 04:44:17 -0700

    0x15-file_io

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -20,11 +20,10 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 
 	for (i = 0; text_content[i] != '\0'; i++)
-		;
-	status = write(fd, text_content, i);
+		;status = write(fd, text_content, i);
 	if (status == -1)
 		return (-1);
 
 	close(fd);
 	return (1);
-
+}
